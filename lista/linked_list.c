208fb5f54e1fe6f507079590302b8a8d957ae1f4
Avila <guilherme.medeiros.avila@gmail.com>
2023-06-10 20:46:17 -0300

    commit_linkedList Function: printList and main function

--- lista/linked_list.c
+++ lista/linked_list.c
@@ -39,3 +39,31 @@ void addFirst(LinkedList *L, int value){
     L->begin = no;
 
 }
+
+void printList(const LinkedList *L){
+    
+    // Primeiro nó
+    Node *pNode = L->begin;
+
+    printf("L -> ");
+
+    // Enquanto pNode não aponta para um elemento NULL
+    while(pNode != NULL){
+        printf("%d -> ", pNode->value);
+        pNode = pNode->next;
+    }
+
+    printf("NULL\n");
+}
+
+int main(){
+    LinkedList *L = createLinkedList();
+
+    for(int i=1; i<6; i++){
+        addFirst(L, i);
+    }
+
+    puts("\n");
+    printList(L);
+
+}
